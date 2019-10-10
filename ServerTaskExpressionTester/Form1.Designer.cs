331aac0923cfb4e2fe0ff3afab40367ca3332ab6
Jesse Houwing <jesse.houwing@gmail.com>
2019-10-10 13:21:16 +0200

    Fixes layout issues
    Adds scrollbars
    Fixes control order

--- ServerTaskExpressionTester/Form1.Designer.cs
+++ ServerTaskExpressionTester/Form1.Designer.cs
@@ -47,11 +47,11 @@
             // 
             this.responseEditor.Dock = System.Windows.Forms.DockStyle.Bottom;
             this.responseEditor.Font = new System.Drawing.Font("Consolas", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.responseEditor.Location = new System.Drawing.Point(0, 1075);
-            this.responseEditor.Margin = new System.Windows.Forms.Padding(7, 7, 7, 7);
+            this.responseEditor.Location = new System.Drawing.Point(0, 447);
             this.responseEditor.Multiline = true;
             this.responseEditor.Name = "responseEditor";
-            this.responseEditor.Size = new System.Drawing.Size(1262, 279);
+            this.responseEditor.ScrollBars = System.Windows.Forms.ScrollBars.Both;
+            this.responseEditor.Size = new System.Drawing.Size(1189, 127);
             this.responseEditor.TabIndex = 0;
             this.responseEditor.TextChanged += new System.EventHandler(this.textBox1_TextChanged);
             // 
@@ -60,10 +60,10 @@
             this.expressionEditor.Dock = System.Windows.Forms.DockStyle.Fill;
             this.expressionEditor.Font = new System.Drawing.Font("Consolas", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
             this.expressionEditor.Location = new System.Drawing.Point(0, 0);
-            this.expressionEditor.Margin = new System.Windows.Forms.Padding(7, 7, 7, 7);
             this.expressionEditor.Multiline = true;
             this.expressionEditor.Name = "expressionEditor";
-            this.expressionEditor.Size = new System.Drawing.Size(1262, 1072);
+            this.expressionEditor.ScrollBars = System.Windows.Forms.ScrollBars.Both;
+            this.expressionEditor.Size = new System.Drawing.Size(1189, 343);
             this.expressionEditor.TabIndex = 2;
             this.expressionEditor.TextChanged += new System.EventHandler(this.textBox2_TextChanged);
             // 
@@ -76,33 +76,37 @@
             this.Key,
             this.Value});
             this.dataGridView1.Dock = System.Windows.Forms.DockStyle.Bottom;
-            this.dataGridView1.Location = new System.Drawing.Point(0, 844);
-            this.dataGridView1.Margin = new System.Windows.Forms.Padding(7, 7, 7, 7);
+            this.dataGridView1.Location = new System.Drawing.Point(0, 344);
             this.dataGridView1.Name = "dataGridView1";
-            this.dataGridView1.Size = new System.Drawing.Size(1262, 228);
+            this.dataGridView1.RowHeadersWidth = 102;
+            this.dataGridView1.Size = new System.Drawing.Size(1189, 102);
             this.dataGridView1.TabIndex = 3;
             this.dataGridView1.CellValueChanged += new System.Windows.Forms.DataGridViewCellEventHandler(this.dataGridView1_CellValueChanged);
             // 
             // Key
             // 
             this.Key.HeaderText = "Key";
+            this.Key.MinimumWidth = 12;
             this.Key.Name = "Key";
             this.Key.ReadOnly = true;
+            this.Key.Width = 250;
             // 
             // Value
             // 
             this.Value.HeaderText = "Value";
+            this.Value.MinimumWidth = 12;
             this.Value.Name = "Value";
+            this.Value.Width = 250;
             // 
             // Statusbar
             // 
             this.Statusbar.ImageScalingSize = new System.Drawing.Size(36, 36);
             this.Statusbar.Items.AddRange(new System.Windows.Forms.ToolStripItem[] {
             this.Status});
-            this.Statusbar.Location = new System.Drawing.Point(0, 1354);
+            this.Statusbar.Location = new System.Drawing.Point(0, 574);
             this.Statusbar.Name = "Statusbar";
-            this.Statusbar.Padding = new System.Windows.Forms.Padding(0, 0, 14, 0);
-            this.Statusbar.Size = new System.Drawing.Size(1262, 42);
+            this.Statusbar.Padding = new System.Windows.Forms.Padding(0, 0, 6, 0);
+            this.Statusbar.Size = new System.Drawing.Size(1189, 22);
             this.Statusbar.TabIndex = 4;
             this.Statusbar.Text = "dfghdfgh";
             // 
@@ -111,7 +115,7 @@
             this.Status.DisplayStyle = System.Windows.Forms.ToolStripItemDisplayStyle.Text;
             this.Status.DoubleClickEnabled = true;
             this.Status.Name = "Status";
-            this.Status.Size = new System.Drawing.Size(132, 37);
+            this.Status.Size = new System.Drawing.Size(59, 17);
             this.Status.Text = "Loading...";
             this.Status.TextAlign = System.Drawing.ContentAlignment.MiddleLeft;
             this.Status.DoubleClick += new System.EventHandler(this.LLog_DoubleClick);
@@ -119,56 +123,58 @@
             // Llog
             // 
             this.Llog.Dock = System.Windows.Forms.DockStyle.Right;
-            this.Llog.Location = new System.Drawing.Point(1265, 0);
-            this.Llog.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
+            this.Llog.Location = new System.Drawing.Point(1190, 0);
+            this.Llog.Margin = new System.Windows.Forms.Padding(1, 1, 1, 1);
             this.Llog.Multiline = true;
             this.Llog.Name = "Llog";
             this.Llog.ReadOnly = true;
             this.Llog.ScrollBars = System.Windows.Forms.ScrollBars.Both;
-            this.Llog.Size = new System.Drawing.Size(641, 1396);
+            this.Llog.Size = new System.Drawing.Size(277, 596);
             this.Llog.TabIndex = 5;
             // 
             // splitter1
             // 
             this.splitter1.Dock = System.Windows.Forms.DockStyle.Right;
-            this.splitter1.Location = new System.Drawing.Point(1262, 0);
+            this.splitter1.Location = new System.Drawing.Point(1189, 0);
+            this.splitter1.Margin = new System.Windows.Forms.Padding(1, 1, 1, 1);
             this.splitter1.Name = "splitter1";
-            this.splitter1.Size = new System.Drawing.Size(3, 1396);
+            this.splitter1.Size = new System.Drawing.Size(1, 596);
             this.splitter1.TabIndex = 6;
             this.splitter1.TabStop = false;
             // 
             // splitter2
             // 
             this.splitter2.Dock = System.Windows.Forms.DockStyle.Bottom;
-            this.splitter2.Location = new System.Drawing.Point(0, 841);
+            this.splitter2.Location = new System.Drawing.Point(0, 343);
+            this.splitter2.Margin = new System.Windows.Forms.Padding(1, 1, 1, 1);
             this.splitter2.Name = "splitter2";
-            this.splitter2.Size = new System.Drawing.Size(1262, 3);
+            this.splitter2.Size = new System.Drawing.Size(1189, 1);
             this.splitter2.TabIndex = 7;
             this.splitter2.TabStop = false;
             // 
             // splitter3
             // 
             this.splitter3.Dock = System.Windows.Forms.DockStyle.Bottom;
-            this.splitter3.Location = new System.Drawing.Point(0, 1072);
+            this.splitter3.Location = new System.Drawing.Point(0, 446);
+            this.splitter3.Margin = new System.Windows.Forms.Padding(1, 1, 1, 1);
             this.splitter3.Name = "splitter3";
-            this.splitter3.Size = new System.Drawing.Size(1262, 3);
+            this.splitter3.Size = new System.Drawing.Size(1189, 1);
             this.splitter3.TabIndex = 8;
             this.splitter3.TabStop = false;
             // 
             // Form1
             // 
-            this.AutoScaleDimensions = new System.Drawing.SizeF(14F, 29F);
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(1906, 1396);
+            this.ClientSize = new System.Drawing.Size(1467, 596);
+            this.Controls.Add(this.expressionEditor);
             this.Controls.Add(this.splitter2);
             this.Controls.Add(this.dataGridView1);
-            this.Controls.Add(this.expressionEditor);
             this.Controls.Add(this.splitter3);
             this.Controls.Add(this.responseEditor);
             this.Controls.Add(this.Statusbar);
             this.Controls.Add(this.splitter1);
             this.Controls.Add(this.Llog);
-            this.Margin = new System.Windows.Forms.Padding(7, 7, 7, 7);
             this.Name = "Form1";
             this.Text = "Build & Release Expression Tester";
             this.FormClosing += new System.Windows.Forms.FormClosingEventHandler(this.Form1_FormClosing);
